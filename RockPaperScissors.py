e983df693091a3d78c1d8ddd0942ed4ac38d32ed
Hanif <amrhanif15@gmail.com>
2023-09-03 22:18:13 +0700

    add new file, moveFile.py

---
+++ RockPaperScissors.py
@@ -0,0 +1,109 @@
+import random
+from time import sleep
+from clearAndLoading import Loading, Typing
+
+# loading object instance
+clearLoad = Loading(1, 3, 0.3)
+
+
+# loading animation and clear the screen before
+clearLoad.loadingAnimation()
+
+# the player, can input their own user_name 
+user_name = input("Enter your name: ")
+
+# loading animation and clear the screen before
+clearLoad.loadingAnimation()
+
+
+# all bunch of variables is stored in here :)
+# this is the answer choice
+game_list = ['Rock', 'Paper', 'Scissors'] 
+
+# the computer_score variable
+computer_score = 0
+
+# the player_score variable
+player_score = 0 
+
+# a string that we want to print, when the player won from the computer
+player_won = f"\n{user_name} Won!\n"
+
+# a string that we want to print, when the computer won from the player 
+computer_won = "\nComputer Won!\n" 
+
+# a string that we want to print, if the computer and the player is tied up
+tie = "\nTie Game !!\n"
+
+
+# start the loop
+run = True
+
+while run:
+    
+    # this is a variable for printing the main score
+    main_score = f"Main Score: Computer = {computer_score}, {user_name} = {player_score}" 
+
+    # this is for counting the length of the main_score variable
+    main_score_line = len(main_score) 
+
+    # just a line, for separating line by line (make the output looks more cleaner)  
+    
+    # so, I times the line with the main score length, because it makes the line looks more efficient and clean :)
+    upper_line = "=" * main_score_line 
+    down_line = "-" * main_score_line 
+    
+    print(f"{upper_line}\n{main_score}\n{down_line}")
+    computer_choice = random.choice(game_list)
+    player_choice = input("Rock, Paper, Scissors, or Quit: ").capitalize() 
+    if player_choice == computer_choice:
+        print(tie)
+
+    elif player_choice == "Rock":
+        if computer_choice == "Scissors":
+            print(player_won)
+            player_score += 1
+
+        else:
+            print(computer_won)
+            computer_score += 1
+
+    elif player_choice == "Paper":
+        if computer_choice == "Rock":
+            print(player_won)
+            player_score += 1
+
+        else:
+            print(computer_won)
+            computer_score += 1
+
+    elif player_choice == "Scissors":
+        if computer_choice == "Paper":
+            print(player_won)
+            player_score += 1
+
+        else:
+            print(computer_won)
+            computer_score += 1
+
+    elif player_choice == "Quit":
+
+       # loading animation and clear the screen before
+        clearLoad.loadingAnimation()
+
+        retype = Typing("Bye .. Bye ..", 3, 0.2, 0.3, False)
+        retype.typeAnimation()
+        break
+
+    else:
+        print("\nInvalid Input !\n")
+        player_choice = "(Input Error)"
+    
+    print(f"- {user_name}: {player_choice}")
+    print(f"- Computer: {computer_choice}\n")
+
+    input("\npress ENTER for continue")
+
+    # loading animation and clear the screen before
+    clearLoad.loadingAnimation()
+
---
+++ synonym_antonym.py
@@ -0,0 +1,121 @@
+# If you want to copy this code, make sure that you have install the PyDictionary module
+# Install ---> pip install PyDictionary
+
+# import the PyDictionary module
+from PyDictionary import PyDictionary
+
+from time import sleep 
+from clearAndLoading import Loading, Typing
+
+
+# make the instance for PyDictionary
+dictionary = PyDictionary()
+
+# instance for the clearing stuff
+clearLoad = Loading(0, 3, 0.3)
+
+
+def welcome(greeting, line, warning, lineLong):
+    # for clear the entire screen before
+    clearLoad.loadingAnimation()
+
+    # I have make this function a lot cleaner, than before
+    print(line + greeting + line + "\n" + warning + "\n")
+
+    input("\n" + " " * round(1/3 * lineLong) + "press ENTER for continue")
+
+
+def choice():
+
+    # for clear the entire screen before
+    clearLoad.loadingAnimation()
+
+    choice_types = input('What do you want to search from a word?\n1. Synonyms\n2. Antonyms\n\n(press ENTER for Exit)\n\njust choose the number: ')
+    print()
+    if choice_types == '1' or choice_types.capitalize() == "Synonyms":
+        synonym()
+    elif choice_types == '2' or choice_types.capitalize() == "Antonyms":
+        antonym()
+    elif choice_types == '' or choice_types == "Exit":
+        exit_out()
+    else:
+        print('Invalid Input')
+        # for clear the entire screen before
+        clearLoad.loadingAnimation()
+
+        # back to the choice function if user type in the wrong input
+        choice()
+        
+
+def synonym():
+    # for clear the entire screen before
+    clearLoad.loadingAnimation()
+
+    word = input('Input the word that you want to search its synonym: ')
+    result = dictionary.synonym(word)
+    print(f'\nSo the synonym of {word} is:')
+
+    # I'm using for in loop, for looping the answer. It will make it more tidy and cleaner to look
+    for answer in result:
+        print('-', answer)
+
+    # confirmation from the user
+    input("\npress ENTER to continue")
+
+    choice()
+    # back to the choice function
+
+
+def antonym():
+    # for clear the entire screen before
+    clearLoad.loadingAnimation()
+
+    word = input('Input the word that you want to search its antonym: ')
+    result = dictionary.antonym(word)
+    print(f'\nSo the antonym of {word} is:')
+    # I'm using for in loop, for looping the answer. Because, it will make it more tidy and cleaner to look
+    for answer in result:
+        print('-', answer)
+
+    # confirmation from the user
+    input("\npress ENTER to continue")
+
+    choice()
+    # back to the choice function
+
+
+def exit_out():
+    
+    clearLoad.loadingAnimation()
+
+    retype = Typing("Thanks for using this program :)", 3, 0.1, 0.3, True)
+    retype.typeAnimation()
+
+    # for clear the entire screen before
+    clearLoad.clear()
+
+    # program get close
+
+
+# loading intro
+clearLoad.loadingAnimation()
+
+# the user can type in their name
+name = input('Type in your name: ')
+
+# the greeting message, I turn it into a variabel because I want to find the length of the greeting message
+greeting = f"\nWelcome {name}, this program was made for people who want to search an antonym or synonym of a word\n"
+
+# total length of the greeting string
+lineLong = len(greeting) - 2
+
+# this is the cool part. I make the line can be adjust automaticly, it means the line will print as many as the length of the greeting message
+line = '=' * lineLong
+indent = " " * round((1/4 * lineLong))
+
+# warning message
+warning = indent + '*warning, you just can input one word! not more'
+
+# called the function
+welcome(greeting, line, warning, lineLong)
+choice()
